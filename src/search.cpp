4233f5bba44ca93c6c268e75c8ab8d80647b43fc
J. Oster <osterj165@googlemail.com>
2020-07-19 10:10:43 +0200

    Fix low plies. Bench for build stockfish: 4772754

--- src/search.cpp
+++ src/search.cpp
@@ -1002,6 +1002,7 @@ moves_loop: // When in check, search starts from here
 
       // Step 13. Pruning at shallow depth (~200 Elo)
       if (  !rootNode
+          && thisThread->rootDepth > 4
           && pos.non_pawn_material(us)
           && bestValue > VALUE_TB_LOSS_IN_MAX_PLY)
       {
