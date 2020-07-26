841301e1d67fb88c6d1fe2934887a7943b1ecf39
J. Oster <osterj165@googlemail.com>
2020-07-26 11:20:09 +0200

    Formatting.

--- src/eval/nnue/architectures/k-p-cr_256x2-32-32.h
+++ src/eval/nnue/architectures/k-p-cr_256x2-32-32.h
@@ -1,24 +1,21 @@
 // Definition of input features and network structure used in NNUE evaluation function
-
 #ifndef K_P_CR_256X2_32_32_H
 #define K_P_CR_256X2_32_32_H
 
+#include "../features/castling_right.h"
 #include "../features/feature_set.h"
 #include "../features/k.h"
 #include "../features/p.h"
-#include "../features/castling_right.h"
-
-#include "../layers/input_slice.h"
 #include "../layers/affine_transform.h"
 #include "../layers/clipped_relu.h"
+#include "../layers/input_slice.h"
 
 namespace Eval {
-
   namespace NNUE {
 
     // Input features used in evaluation function
     using RawFeatures = Features::FeatureSet<Features::K, Features::P,
-      Features::CastlingRight>;
+                                             Features::CastlingRight>;
 
     // Number of input feature dimensions after conversion
     constexpr IndexType kTransformedFeatureDimensions = 256;
@@ -30,12 +27,10 @@ namespace Eval {
       using HiddenLayer1 = ClippedReLU<AffineTransform<InputLayer, 32>>;
       using HiddenLayer2 = ClippedReLU<AffineTransform<HiddenLayer1, 32>>;
       using OutputLayer = AffineTransform<HiddenLayer2, 1>;
-
-    }  // namespace Layers
+    } // namespace Layers
 
     using Network = Layers::OutputLayer;
+  } // namespace NNUE
+} // namespace Eval
 
-  }  // namespace NNUE
-
-}  // namespace Eval
 #endif // K_P_CR_256X2_32_32_H
